6da0fd381a69144cef52905593e023d4b71255d2
Luis Flores <lflores@headstorm.com>
2023-06-10 11:04:40 -0500

    Configured dot_config template.

--- .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
+++ .chezmoiscripts/darwin/run_onchange_before_install-packages.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+echo "Hello!"
+
 set -eufo pipefail
 
+# Removed unstable install of hombrew
 # Install Homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --no-lock --file=/dev/stdin <<EOF
-
 # Brews
 brew "asciinema"
 brew "bash"
 brew "bash-completion@2"
+brew "chezmoi"
 brew "gh"
 brew "git"
 brew "htop"
@@ -28,21 +31,20 @@ brew "tree"
 brew "watch"
 
 # Casks
-# cask "qflipper"
-cask "1password"
-cask "arc"
+# cask "1password" # Installed manually
 cask "appcleaner"
+# cask "arc" # Installed manually
 cask "daisydisk"
-# cask "docker"
+# cask "docker" # Installed manually
 cask "drawio"
 cask "iterm2"
-# cask "logitech-g-hub"
+# cask "logitech-g-hub" # Installed manually
 cask "monitorcontrol"
 cask "obs"
+cask "qflipper"
 cask "raindropio"
 cask "spotify"
 cask "sublime-text"
 cask "visual-studio-code"
 cask "warp"
-
 EOF
